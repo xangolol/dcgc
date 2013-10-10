1a3070f093ced5c73c0741bf46df60f7a982542c
xangolol <superxango@hotmail.com>
2013-10-10 19:06:37 +0200

    Create expenses controller files

---
+++ app/controllers/expenses_controller.rb
@@ -0,0 +1,4 @@
+class ExpensesController < ApplicationController
+	#TODO make create, new and index methods
+	#TODO make sample data
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ DcgcApp::Application.routes.draw do
   resources :events
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:show, :edit, :update]
+  resources :groceries, controller: 'expenses'
   root "static_pages#home"
   match '/login', to: 'sessions#new', via: 'get'
   match '/logout', to: 'sessions#destroy', via: 'delete'
